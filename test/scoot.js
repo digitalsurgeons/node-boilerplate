b7401e7075b6ae13723b2551b4fd13f20210d20b
jekrb <Jacob.JW.Burden@gmail.com>
2016-06-28 14:19:19 -0400

    added unit test for scoot.js

---
+++ test/scoot.js
@@ -0,0 +1,28 @@
+const scoot = require('../lib/scoot');
+const test = require('tape');
+const through = require('through2');
+const split = require('split2');
+const stream = through();
+
+test('simple', t => {
+  t.plan(1);
+
+  const sp = stream.pipe(split(JSON.parse));
+  sp.on('error', err => {
+    t.fail(err);
+  });
+
+  const so = scoot('test');
+
+  so(stream, 'pass?');
+
+  sp.pipe(through.obj((row, enc, next) => {
+    if (!row) next();
+
+    if (row.test) {
+      t.same(row.test, 'pass?');
+    }
+
+    next();
+  }));
+});
