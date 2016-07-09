5e972a97b3dd5af8bf5df3eb06fd1b3247024f1e
jekrb <Jacob.JW.Burden@gmail.com>
2016-07-09 01:23:13 -0400

    linting

--- browser/js/index.js
+++ browser/js/index.js
@@ -1,5 +1,5 @@
-const through = require('through2')
-const split = require('split2')
+const through = require('through2');
+const split = require('split2');
 const websocket = require('websocket-stream');
 const level = require('level');
 const scoot = require('scoot-stream');
@@ -7,27 +7,27 @@ const scoot = require('scoot-stream');
 const db = level({
   valueEncoding: 'json',
   keyEncoding: require('bytewise')
-})
+});
 
 db.createReadStream({
   gt: 'cats!',
   lt: 'cats!~'
 }).on('data', data => {
-  console.log(data)
+  console.log(data);
 });
 
 const ui = {
   header: document.querySelector('header'),
   input: document.querySelector('input'),
   page: document.querySelector('.page')
-}
+};
 
 const ws = websocket('ws://localhost:9090');
 const sp = ws.pipe(split(JSON.parse));
 const so = scoot('msg');
 
 sp.on('error', err => {
-  console.error(err)
+  console.error(err);
 });
 
 sp.pipe(through.obj(write));
@@ -36,7 +36,7 @@ so(ws, 'hello from the client');
 
 so(ws, {
   read: 'cats'
-})
+});
 
 ui.input.addEventListener('keydown', ev => {
   if (ev.keyCode === 13) {
@@ -49,9 +49,9 @@ ui.input.addEventListener('keydown', ev => {
     so(ws, {
       broadcast: value,
       cookie: document.cookie.split('session=')[1]
-    })
+  });
   }
-})
+});
 
 const ul = document.createElement('ul');
 ui.page.appendChild(ul);
@@ -74,10 +74,9 @@ function write (row, enc, next) {
     if (row.key.indexOf('cats!') === 0) {
       db.put(row.key, row.value, err => {
         if (err) console.error(err);
-      })
+	});
     }
   }
 
   next();
 }
-
